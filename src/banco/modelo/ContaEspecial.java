4a04f316a9e0af8cb6dc5d853f89073a9f1ee525
Giselle Souza <giselle_souza97@yahoo.com.br>
2023-12-05 22:44:37 -0300

    Heranca
    Permite criar novas classes(classes filhas) baseadas em classes existentes (superclasses), aproveitando os atributos e métodos já definidos na classe pai.

---
+++ src/banco/modelo/ContaEspecial.java
@@ -0,0 +1,21 @@
+package banco.modelo;
+
+public class ContaEspecial extends Conta {
+	
+	private double valorLimite;
+
+	public ContaEspecial(Pessoa titular, int agencia, int numero, double valorLimite) {
+		super(titular, agencia, numero);
+		this.valorLimite = valorLimite;
+	}
+
+	public double getValorLimite() {
+		return valorLimite;
+	}
+
+	public void setValorLimite(double valorLimite) {
+		this.valorLimite = valorLimite;
+	}
+	
+	
+}
